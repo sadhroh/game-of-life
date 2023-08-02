23d6017c3c56085832669102aa2779bc287a3c72
Rohit Sadhukhan <30411932+sadhroh@users.noreply.github.com>
2023-08-02 16:05:45 +0530

    main with generation init

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"time"
 )
 
-func main(){
-	fmt.Println("Hello world")
+// command line flags
+var (
+	gameSeed         = flag.Int64("seed", time.Now().UnixNano(), "starting seed")
+	gameWorldRows    = flag.Int("rows", 40, "number of rows")
+	gameWorldColumns = flag.Int("columns", 40, "number of columns")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	universe := NewWorld(*gameSeed, *gameWorldRows, *gameWorldColumns)
+	universe.Show()
+	universe.Next()
 }
